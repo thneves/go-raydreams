d1a0b48c94a4eee6a772889eaf909f125d283448
thneves <thales.neves@pinpeople.com.br>
2025-08-23 22:18:02 -0300

    add reamining lines counter

--- main.go
+++ main.go
@@ -1,25 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
 	// IMAGE
 
-	image_width := 256
-	image_height := 256
+	imageWidth := 256
+	imageHeight := 256
 
-	fmt.Printf("P3\n%d %d\n255\n", image_width, image_height)
+	fmt.Printf("P3\n%d %d\n255\n", imageWidth, imageHeight)
 
 	// RENDER
 
 	// Required Header For a PPM file
-	// The P3 means colors are in ASCII, then 255 columns and 255 rows
+	// The P3 means colors are in ASCII, then 256 columns and 256 rows
 
-	for i := 0; i < image_height; i++ {
-		for j := 0; j < image_width; j++ {
-			r := float64(j) / float64(image_width-1)
-			g := float64(i) / float64(image_height-1)
+	for i := 0; i < imageHeight; i++ {
+
+		remaining := imageHeight - i
+		fmt.Fprintf(os.Stderr, "\rScanlines remaining: %d", remaining)
+
+		for j := 0; j < imageWidth; j++ {
+			r := float64(j) / float64(imageWidth-1)
+			g := float64(i) / float64(imageHeight-1)
 			b := 0.0
 			ir := int(255.999 * r)
 			ig := int(255.999 * g)
@@ -28,4 +35,5 @@ func main() {
 			fmt.Printf("%d %d %d\n", ir, ig, ib)
 		}
 	}
+	fmt.Fprintln(os.Stderr)
 }

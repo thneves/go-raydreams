11bf15adb3a71c0897dfaedd366ebfb2259e0f1a
thneves <thales.neves@pinpeople.com.br>
2025-08-25 22:22:19 -0300

    add red sphere to the image :D

--- scene/ray.go
+++ scene/ray.go
@@ -27,7 +27,22 @@ func (ray Ray) At(t float64) Point3 {
 	return Add(ray.origin, MulScalar(ray.direction, t))
 }
 
+func (ray Ray) hitSphere(center Point3, radius float64) bool {
+	oc := Sub(center, ray.origin)
+	a := Dot(ray.direction, ray.direction)
+	b := -2.0 * Dot(ray.direction, oc)
+	c := Dot(oc, oc) - radius*radius
+	discriminant := b*b - 4*a*c
+	return discriminant >= 0
+}
+
 func (ray Ray) RayColor() Color {
+	point3 := Point3{X: 0, Y: 0, Z: -1}
+
+	if ray.hitSphere(point3, 0.5) {
+		return Color{R: 1, G: 0, B: 0}
+	}
+
 	unitDir := Unit(ray.direction)
 	a := 0.5 * (unitDir.Y + 1.0)
 	white := Color{R: 1, G: 1, B: 1}

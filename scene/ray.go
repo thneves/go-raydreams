c8f0f5b7086fddb021085ca194d6a6bd27887a64
thneves <thales.neves@pinpeople.com.br>
2025-08-30 18:33:45 -0300

    delete hit method from Ray file

--- scene/ray.go
+++ scene/ray.go
@@ -1,17 +1,9 @@
 package scene
 
-import "math"
-
 // What is a Ray
 type Ray struct {
 	origin    Point3
 	direction Vec3
-}
-
-type HitRecord struct {
-	origin Point3
-	vector Vec3
-	t      float64
 }
 
 func NewRay(origin, direction Vec3) Ray {
@@ -35,44 +27,12 @@ func (ray Ray) At(t float64) Point3 {
 	return Add(ray.origin, MulScalar(ray.direction, t))
 }
 
-func (ray Ray) Hit(center Point3, record HitRecord, rayTmin, rayTmax, radius float64) (t float64, ok bool) {
-	oc := Sub(center, ray.origin)
-
-	a := LenSquared(ray.direction)
-	h := Dot(ray.direction, oc)
-	c := LenSquared(oc) - radius*radius
-	discrimant := h*h - a*c
-
-	if discrimant < 0 {
-		return -1.0, false
-	}
-
-	sqrtd := math.Sqrt(discrimant)
-
-	// Find the neares root that lies in the acceptable range.
-
-	root := (h - sqrtd) / a
-
-	if root <= rayTmin || rayTmax <= root {
-		root := (h + sqrtd) / a
-		if root <= rayTmin || rayTmax <= root {
-			return -1.0, false
-		}
-	}
-
-	record.t = root
-	record.origin = ray.At(record.t)
-	record.vector = DivScalarPointer(SubPointer(record.origin, center), radius)
-
-	return 1.0, true
-}
-
 func (ray Ray) RayColor() Color {
 	center := Point3{X: 0, Y: 0, Z: -1}
 
-	t := ray.Hit(center, 0.5)
+	t, ok := ray.Hit(center, 0.5)
 
-	if t > 0.0 {
+	if ok {
 		n := Unit(Sub(ray.At(t), center))
 		return MulScalarColors(Color{n.X + 1, n.Y + 1, n.Z + 1}, 0.5)
 	}
